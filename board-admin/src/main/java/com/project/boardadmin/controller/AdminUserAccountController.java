c2822438a9c5b81a66be4cb92b89671872f0a233
김민성 <minsungk@uangel.com>
2024-08-28 16:20:41 +0900

    컨트롤러 기본 세팅

--- board-admin/src/main/java/com/project/boardadmin/controller/AdminUserAccountController.java
+++ board-admin/src/main/java/com/project/boardadmin/controller/AdminUserAccountController.java
@@ -1,10 +1,22 @@
 package com.project.boardadmin.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/admin/user-account")
+@RequestMapping("/admin/members")
 @Controller
 public class AdminUserAccountController {
 
+    @GetMapping
+    public String userAccounts(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            Model model
+    ) {
+        return "admin/members";
+    }
 }
