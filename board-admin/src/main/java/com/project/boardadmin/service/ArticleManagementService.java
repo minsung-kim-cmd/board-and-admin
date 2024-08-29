f066ecf108195c754d5f9fe84bc4740cd343ca5a
김민성 <minsungk@uangel.com>
2024-08-29 09:49:59 +0900

    게시글 관리 페이지 기능 테스트 정의

---
+++ board-admin/src/main/java/com/project/boardadmin/service/ArticleManagementService.java
@@ -0,0 +1,25 @@
+package com.project.boardadmin.service;
+
+import com.project.boardadmin.dto.ArticleDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleManagementService {
+
+    public List<ArticleDto> getArticles() {
+        return List.of();
+    }
+
+    public ArticleDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void deleteArticle(Long articleId) {
+
+    }
+
+}
