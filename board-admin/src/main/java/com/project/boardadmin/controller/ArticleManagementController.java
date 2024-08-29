36dbe89c66e7d6a53f7e5ae46ffb55fd8dd4c666
김민성 <minsungk@uangel.com>
2024-08-29 15:18:30 +0900

    게시글 관리 페이지 구현

--- board-admin/src/main/java/com/project/boardadmin/controller/ArticleManagementController.java
+++ board-admin/src/main/java/com/project/boardadmin/controller/ArticleManagementController.java
@@ -1,22 +1,37 @@
 package com.project.boardadmin.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import com.project.boardadmin.dto.response.ArticleResponse;
+import com.project.boardadmin.service.ArticleManagementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/articles")
 @Controller
 public class ArticleManagementController {
 
-    @GetMapping
-    public String articles(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+    private final ArticleManagementService articleManagementService;
+
+    public String articles(Model model) {
+        model.addAttribute(
+                "articles",
+                articleManagementService.getArticles().stream().map(ArticleResponse::withoutContent).toList()
+        );
         return "management/articles";
+    }
+
+    @ResponseBody
+    @GetMapping("/{articleId}")
+    public ArticleResponse article(@PathVariable Long articleId) {
+        return ArticleResponse.withContent(articleManagementService.getArticle(articleId));
+    }
+
+    @PostMapping("/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:/management/articles";
     }
 }
