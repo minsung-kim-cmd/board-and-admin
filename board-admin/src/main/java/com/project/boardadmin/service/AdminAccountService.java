e3855812f0b87bb0ddac49c3ff4c623081b5cc44
김민성 <minsungk@uangel.com>
2024-08-29 17:21:59 +0900

    로그인 페이지 구현

--- board-admin/src/main/java/com/project/boardadmin/service/AdminAccountService.java
+++ board-admin/src/main/java/com/project/boardadmin/service/AdminAccountService.java
@@ -6,11 +6,13 @@ import com.project.boardadmin.dto.AdminAccountDto;
 import com.project.boardadmin.repository.AdminAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class AdminAccountService {
